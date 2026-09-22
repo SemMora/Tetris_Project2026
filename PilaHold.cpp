577485608cd9b1bdd438da43aad46438e3342c7d
SemMora <semmora04@gmail.com>
2026-09-22 13:35:42 -0600

    Cambio de Vector a Struct en la Pila
    Cambié la pila del hold para que use un struct NodoPila con un puntero al tope, igual que la cola, en vez de un arreglo, ya que las pilas van sólo con structs y nodos por más que un vector tamaño 1 sea relativamente más eficiente o sencillo de realizar

---
+++ PilaHold.cpp
@@ -0,0 +1,53 @@
+#include "PilaHold.h"
+
+PilaHold::PilaHold() {
+	tope = nullptr;
+	cantidad = 0;
+}
+
+PilaHold::~PilaHold() {
+	vaciar();
+}
+
+bool PilaHold::apilar(int tipo) {
+	if (estaLlena()) {
+		return false;
+	}
+	NodoPila* nuevo = new NodoPila(tipo,tope);
+	tope = nuevo;
+	cantidad++;
+	return true;
+}
+
+int PilaHold::desapilar() {
+	if (estaVacia()) {
+		return -1;
+	}
+	NodoPila* viejo = tope;
+	int tipo = viejo->tipo;
+	tope = tope->siguiente;
+	delete viejo;
+	cantidad--;
+	return tipo;
+}
+
+int PilaHold::verTope() const {
+	if (estaVacia()) {
+		return -1;
+	}
+	return tope->tipo;
+}
+
+bool PilaHold::estaVacia() const {
+	return tope == nullptr;
+}
+
+bool PilaHold::estaLlena() const {
+	return cantidad == CAPACIDAD;
+}
+
+void PilaHold::vaciar() {
+	while (!estaVacia()) {
+		desapilar();
+	}
+}
--- PilaHold.h
+++ PilaHold.h
@@ -1,4 +1,33 @@
 #ifndef PILAHOLD_H
 #define PILAHOLD_H
 
+// Nodo de la pila: guarda el tipo de la pieza en espera (0 al 6)
+struct NodoPila {
+	int tipo;
+	NodoPila* siguiente;  
+};
+
+// Pila de capacidad 1 para la pieza en espera (hold).
+class PilaHold {
+private:
+	static const int CAPACIDAD = 1;
+	NodoPila* tope;
+	int cantidad;
+	
+public:
+	PilaHold();
+	~PilaHold();
+	
+	bool apilar(int tipo);    // devuelve false si ya esta llena la pila
+	int desapilar();          //  devuelve un -1 como codigo de error si esta vaciala pila
+	int verTope() const;      
+	bool estaVacia() const;
+	bool estaLlena() const;
+	void vaciar();
+	
+	//metodos para no copiar la pila y poder liberarla de forma eficiente
+	PilaHold(const PilaHold&) = delete;
+	PilaHold& operator=(const PilaHold&) = delete;
+};
+
 #endif
